68698882d537e20ba0bccea2687e7717360f1c71
Muhammad Alif Danial <aldmnsri@gmail.com>
2018-12-31 23:16:17 +0800

    Revert "initial commit"
    This reverts commit a0e5e0e875b79bf3a731f5fe8e9c5f2cc244a575.

--- MemeWordsAnalytics.py
+++ MemeWordsAnalytics.py
@@ -5,44 +5,27 @@ import string
 import array as arr
 
 number_list = list(range(1, 27))
+print(number_list)
 alphabet_list = list(string.ascii_lowercase)
-'''for x in number_list:
-    print("%d : %c" % (x, alphabet_list[x-1]))'''
-for x in number_list:
-    print(x, end=" ")
-print("")
-for y in alphabet_list:
-    print(y, end=" ")
-print("")
-
+print(alphabet_list)
 a = arr.array('i', [])
+
 word = input("Enter an adjective: ")
 word = word.replace(" ", "")
+
 for x in word:
     for y in alphabet_list:
-        z = alphabet_list.index(x) + 1
+        z = alphabet_list.index(x)+1
         a.append(z)
         break
-print(a)
+
 Sum = sum(a)
 print("%s = %d" % (word, Sum))
 
-'''
-a = arr.array('i', [])
-f1 = open("adjectives.txt", "r")
-for w in f1:
-    word = w.replace("\n", "")
-    for x in word:
-        for y in alphabet_list:
-            z = alphabet_list.index(x)+1
-            a.append(z)
-            break
-    f = open("WordAnalytics.txt", "a+")
-    Sum = sum(a)
-    a = []
-    word = w.replace("\n", "")
-    if Sum != 0:
-        f.write("%s = %d \n" % (word, Sum))
-    f.close()
-    f1.close()'''
+
+
+
+
+
+
 
