b82ebf489eb2d9013e86ea52b1e66d1f10e9e2f2
Muhammad Alif Danial <aldmnsri@gmail.com>
2018-12-31 23:24:00 +0800

    Workable commit
    left undone part in comments at bottom

--- MemeWordsAnalytics.py
+++ MemeWordsAnalytics.py
@@ -5,10 +5,16 @@ import string
 import array as arr
 
 number_list = list(range(1, 27))
-print(number_list)
 alphabet_list = list(string.ascii_lowercase)
-print(alphabet_list)
 a = arr.array('i', [])
+'''for x in number_list:
+    print("%d : %c" % (x, alphabet_list[x-1]))'''
+for x in number_list:
+    print(x, end=" ")
+print("")
+for y in alphabet_list:
+    print(y, end=" ")
+print("")
 
 word = input("Enter an adjective: ")
 word = word.replace(" ", "")
@@ -18,9 +24,28 @@ for x in word:
         z = alphabet_list.index(x)+1
         a.append(z)
         break
-
+print(a)
 Sum = sum(a)
 print("%s = %d" % (word, Sum))
+
+'''
+a = arr.array('i', [])
+f1 = open("adjectives.txt", "r")
+for w in f1:
+    word = w.replace("\n", "")
+    for x in word:
+        for y in alphabet_list:
+            z = alphabet_list.index(x)+1
+            a.append(z)
+            break
+    f = open("WordAnalytics.txt", "a+")
+    Sum = sum(a)
+    a = []
+    word = w.replace("\n", "")
+    if Sum != 0:
+        f.write("%s = %d \n" % (word, Sum))
+    f.close()
+    f1.close()'''
 
 
 
